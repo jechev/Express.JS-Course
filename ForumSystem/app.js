7dd06b8906eba687d0f148cbd0fd447a54d94c9a
Dimitar Zhechev <dimityr.jechev@gmail.com>
2016-11-03 11:08:52 +0200

    Add all need packages and typings

---
+++ ForumSystem/app.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const express = require('express')
+
+mongoose.Promise = global.Promise
+let app = express()
+app.get('/', (req, res) => {
+  console.log('Express ready!')
+  mongoose
+    .connect('mongodb://localhost:27017/blogsystem')
+    .then(() => {
+      console.log('MongoDB ready!')
+      res.send('OK!')
+    })
+})
+app.listen(1337)
